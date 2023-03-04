59b7123a3740079b037ab744dd75442057b9491c
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-03-04 17:31:04 -0300

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,107 +1,93 @@
-const Container = document.querySelector('.Container');
-const IAg = Container.querySelector('.InputAg');
-const IConta = Container.querySelector('.InputConta');
-const Botao = Container.querySelector('.Botao');
-const div = Container.querySelector('div');
-//superclasse
-function Conta(Agencia, Conta, Saldo){
+function Conta(Agencia, CConta, Saldo){ 
     this.Agencia = Agencia;
-    this.Conta = Conta;
-    Object.defineProperty(this, 'Saldo',{
-        enumerable:true,
-        configurable:true,
-        get: function(){
-            Saldo = Math.floor(Math.random() * this.Agencia);
-            return Saldo;
-        },
-    });
+    this.CConta = CConta;
+    this.Saldo = Saldo;
+    this.Container = document.querySelector('.Container');
+    this.eventos();
 }
-Conta.prototype.sacar = function(){
-    if(input.value>this.Saldo){
-        alert('Opa! Saldo insuficiente!');
+Conta.prototype.eventos = function(){
+    this.Container.addEventListener('click', e=>{
+        const el = e.target;
+        if(el.classList.contains('Botao')) this.receberDados();
+        if(el.classList.contains('btnSaque')){
+          if(this.tipagem == 'CC'){
+            CC.saque(this.input.value);
+            }else{
+            this.saque(this.input.value);};
+        if(el.classList.contains('btnDep')) this.deposito(this.input.value);     
+}});};
+Conta.prototype.tipoConta = function(){
+    this.tipagem = this.Agencia > 100 ? 'CC' : 'CP';
+    return this.tipagem;
+};
+Conta.prototype.opcoes = function(){
+    this.IAg.value = '';
+    this.IConta.value = '';
+    this.apresentarDado(`Agencia(Ag): ${this.Agencia} | Conta: ${this.CConta}
+     | Tipo da conta: ${this.tipoConta()} | Saldo: R$${this.Saldo}`);
+    this.inputNew();
+    this.btnSaque();
+    this.btnDep();
+};
+Conta.prototype.receberDados = function(){
+     this.IAg = this.Container.querySelector('.InputAg');
+     this.IConta = this.Container.querySelector('.InputConta');
+     this.Agencia =  this.IAg.value;
+     this.CConta =  this.IConta.value;
+     this.Saldo = Math.floor(Math.random() * this.Agencia);
+     this.opcoes();
+};
+Conta.prototype.saque = function(valor){
+    if(valor>this.Saldo){
+        //Por um callBack
+        this.apresentarDado(`Saldo insuficiente! Por favor, tente novamente com outros valores ;)`);
         return;
-    }
-    this.Saldo -= input.value;
-    mostrarSaldo();
+    }else{
+        this.Saldo -= valor;
+        this.apresentarDado(`Tudo certo! Novo Saldo: R$${this.Saldo}!`);
+}};
+Conta.prototype.deposito = function(valor){
+        //Por um callBack
+        this.Saldo += Number(valor);
+        this.apresentarDado(`Parabéns! Novo saldo: R$${this.Saldo}`);
 };
-Conta.prototype.depositar = function(){
-    this.Saldo += input.value;
-    mostrarSaldo();
+Conta.prototype.apresentarDado = function(Dado){
+    this.Div = this.Container.querySelector('div');
+    this.p = document.createElement('p');
+    this.Div.appendChild(this.p);
+    this.p.setAttribute('class','Paragrafo');    
+    this.p.innerHTML = Dado;
 };
-Conta.prototype.mostrarSaldo = function(){
-    criaP(`Novo saldo: ${this.Saldo.toFixed(2)}`);
+Conta.prototype.inputNew = function(){
+    this.input = document.createElement('input');
+    this.Div.appendChild(this.input);
+    this.input.setAttribute('class','inputOpcoes');
 };
-function CC(agencia, conta, saldo, limite) {
-    Conta.call(this, agencia, conta, saldo);
+Conta.prototype.btnSaque = function(){
+    this.BtnSaque = document.createElement('button');
+    this.Div.appendChild(this.BtnSaque);
+    this.BtnSaque.innerText = 'Sacar';
+    this.BtnSaque.setAttribute('class','btnSaque');
+};
+Conta.prototype.btnDep = function(){
+    this.BtnDep = document.createElement('button');
+    this.Div.appendChild(this.BtnDep);
+    this.BtnDep.innerText = 'Deposito';
+    this.BtnDep.setAttribute('class','btnDep');
+};
+function CC(agencia, conta, limite) {
+    Conta.call(this, agencia, conta);
     this.limite = limite;
   }
   CC.prototype = Object.create(Conta.prototype);
   CC.prototype.constructor = CC;
-  
-  CC.prototype.sacar = function() {
-    if(input.value > (this.Saldo + this.limite)) {
-      console.log(`Saldo insuficiente: ${this.Saldo}`);
-      return;
-    }
-  
-    this.Saldo -= input.value;
-    this.mostrarSaldo();
-  };
-  
-  function CP(agencia, conta, saldo) {
-    Conta.call(this, agencia, conta, saldo);
-  }
-  CP.prototype = Object.create(Conta.prototype);
-  CP.prototype.constructor = CP;
-  
-//funcoes para colocar no prototype >>
-function telaInicial(algumacoisa){
-    criaP(`Sua conta: ${algumacoisa.tipo} e seu saldo é: ${algumacoisa.Saldo}`);
-    criarInput(div);
-    botaoSacar(div);
-    botaoDepositar(div);
-}
-function criaP(algumacoisa){
-    const p = document.createElement('p');
-    Container.appendChild(p);
-    p.setAttribute('class','Informacoes');
-    p.innerHTML = algumacoisa;
-}
-function botaoDepositar(p){
-    const btnD = document.createElement('button');
-    btnD.innerText =  'Depositar';
-    btnD.setAttribute('class','Deposito');
-    p.appendChild(btnD);
-}
-function botaoSacar(p){
-    const btnS = document.createElement('button');
-    btnS.innerText =  'Sacar';
-    btnS.setAttribute('class','Saque');
-    p.appendChild(btnS);
-}
-function criarInput(p){
-    const input = document.createElement('input');
-    Container.appendChild(input);
-    input.setAttribute('class','InputNovo');
-    p.appendChild(input);
-}
-function criarRandom(valor){
-    return Math.floor(Math.random() * valor);
-}
-function limpar(){
-    IAg.value = '';
-    IConta.focus();
-    IConta.value = '';
-    IConta.focus();
-}
-//<<
-Botao.addEventListener('click', (e)=>{
-    const continha = new Conta(IAg.value, IConta.value)
-    telaInicial(continha);
-});
-Container.addEventListener('click', (e)=>{
-    const sacando = e.target;
-    if(sacando.classList.contains('Saque')){
-        criaP(`${continha.sacar()}`);
-    }
-});
+  CC.prototype.saque = function(valor) {
+    if(valor > (this.saldo + this.limite)) {
+        //Por um callBack
+        this.apresentarDado(`Saldo insuficiente mesmo com o limite! Por favor, tente novamente com outros valores ;)`);
+        return;
+    }else{
+        this.Saldo -= valor;
+        this.apresentarDado(`Tudo certo! Novo Saldo: R$${this.Saldo}!`);
+}};
+const IniciarConta = new Conta();
